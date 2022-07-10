efd828ec58a3cc0e79042acf37325af68eb2d050
vietnguyen1301 <viet1301.ios@gmail.com>
2022-07-10 20:07:25 +0700

    Initial Commit

---
+++ MagicGatheringUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicGatheringUIKit
+//
+//  Created by Nguyễn Việt on 10/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
